51f7dbac684117d32d154dae80f3f40388535ec1
wisberry2022 <wisberry2022@gmail.com>
2024-07-14 11:11:42 +0900

    slip filter bar

--- src/1.pages/slip/ui/InquirySlip.tsx
+++ src/1.pages/slip/ui/InquirySlip.tsx
@@ -1,7 +1,12 @@
+import { FilterBar } from "@/2.widgets/slip";
 import { FC } from "react";
 
 const InquirySlip: FC = () => {
-  return <section id="slip-list">전표</section>;
+  return (
+    <section id="slip-list">
+      <FilterBar />
+    </section>
+  );
 };
 
 export default InquirySlip;
--- src/2.widgets/slip/index.ts
+++ src/2.widgets/slip/index.ts
@@ -1,3 +1,4 @@
 export { ReceiptSlip } from "./ui/receipt-slip/ReceiptSlip";
+export { FilterBar } from "./ui/list-filter-bar/FilterBar";
 export { TransferSlip } from "./ui/transfer-slip/TransferSlip";
 export { WithdrawalSlip } from "./ui/withdrawal-slip/WithdrawalSlip";

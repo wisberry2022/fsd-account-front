5a7cdb7368758dc71aacd455e0864b96921da14f
wisberry2022 <wisberry2022@gmail.com>
2024-07-17 20:04:35 +0900

    css var

--- src/0.app/App.tsx
+++ src/0.app/App.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/AppRouter";
+import "./styles/globals-var.css";
 import "./styles/globals.css";
 import "./styles/globals-color.css";
 

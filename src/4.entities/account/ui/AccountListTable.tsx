f83b77ee633dffe1a41bff307ddf1f4422aaf501
wisberry2022 <wisberry2022@gmail.com>
2024-07-20 11:17:32 +0900

    act tbl mod

--- src/4.entities/account/ui/AccountListTable.tsx
+++ src/4.entities/account/ui/AccountListTable.tsx
@@ -6,9 +6,7 @@ export const AccountListTable: FC = () => {
     <table id="fixed-act-list-tbl">
       <thead>
         <tr>
-          <th className="tbl-head-color">대분류</th>
-          <th className="tbl-head-color">중분류</th>
-          <th className="tbl-head-color">소분류</th>
+          <th className="tbl-head-color">분류</th>
           <th className="tbl-head-color">계정과목명</th>
           <th className="tbl-head-color">코드</th>
         </tr>
@@ -17,8 +15,6 @@ export const AccountListTable: FC = () => {
         {Array.from({ length: 40 }).map((_, i) => (
           <tr key={i}>
             <td>자산</td>
-            <td>유동자산</td>
-            <td>당좌자산</td>
             <td>보통예금</td>
             <td>101-001</td>
           </tr>
--- src/4.entities/account/ui/AccountRegisterTable.tsx
+++ src/4.entities/account/ui/AccountRegisterTable.tsx
@@ -17,30 +17,6 @@ export const AccountRegisterTable: FC = () => {
             </select>
           </td>
         </tr>
-        <tr className="category">
-          <th className="tbl-head-color">중분류</th>
-          <td className="select-td" colSpan={3}>
-            <select className="act-category">
-              <option>자산</option>
-              <option>부채</option>
-              <option>자본</option>
-              <option>수익</option>
-              <option>비용</option>
-            </select>
-          </td>
-        </tr>
-        <tr className="category">
-          <th className="tbl-head-color">소분류</th>
-          <td className="select-td" colSpan={3}>
-            <select className="act-category">
-              <option>자산</option>
-              <option>부채</option>
-              <option>자본</option>
-              <option>수익</option>
-              <option>비용</option>
-            </select>
-          </td>
-        </tr>
         <tr>
           <th className="tbl-head-color">계정과목 이름</th>
           <td>

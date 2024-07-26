d43c6425caf4d68b63a8c83f6058ae617bbaaa90
wisberry2022 <wisberry2022@gmail.com>
2024-07-27 08:27:08 +0900

    slip list header

--- src/1.pages/slip/ui/InquirySlip.tsx
+++ src/1.pages/slip/ui/InquirySlip.tsx
@@ -1,10 +1,21 @@
-import { FilterBar, SlipTable } from "@/2.widgets/slip";
+import { SlipTable } from "@/2.widgets/slip";
+import { usePopover } from "@/5.shared/hooks";
 import { FC } from "react";
+import { FiSettings } from "react-icons/fi";
+import "./css/inquiry-slip.css";
 
 const InquirySlip: FC = () => {
+  const popover = usePopover();
+
   return (
     <section id="slip-list">
-      <FilterBar />
+      <div id="list-header" onClick={popover.onToggle}>
+        <h2>전표 조회</h2>
+        <div className="btn-sky-white">
+          <FiSettings id="setting-icon" />
+          <strong>검색필터</strong>
+        </div>
+      </div>
       <SlipTable />
     </section>
   );
--- src/2.widgets/slip/index.ts
+++ src/2.widgets/slip/index.ts
@@ -1,5 +1,4 @@
 export { ReceiptSlip } from "./ui/slip-write/receipt-slip/ReceiptSlip";
 export { SlipTable } from "./ui/slip-list/SlipTable";
-export { FilterBar } from "./ui/slip-list/list-filter-bar/FilterBar";
 export { TransferSlip } from "./ui/slip-write/transfer-slip/TransferSlip";
 export { WithdrawalSlip } from "./ui/slip-write/withdrawal-slip/WithdrawalSlip";
